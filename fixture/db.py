228183cb41f3bd2201421a9b0674cc0e68c3f84f
akasimova <brskasimova@gmail.com>
2021-02-06 15:35:10 +0300

    Добавлен тест логина

---
+++ fixture/db.py
@@ -0,0 +1,14 @@
+#import pymysql.cursors
+
+
+class DbFixture:
+
+    def __init__(self, host, name, user, password):
+        self.host = host
+        self.name = name
+        self.user = user
+        self.password = password
+        self.connection = connection = pymysql.connect(host=host, database=name, user=user, password=password, autocommit=True)
+
+    def destroy(self):
+        self.connection.close()
